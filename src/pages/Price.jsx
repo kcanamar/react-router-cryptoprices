609cb1873a15fabde40c21a9a382c956b57b65f4
Kyle Canamar <canamar.kyle@gmail.com>
2022-05-14 12:58:18 -0700

    uses the IEX api to populate each price with current data

--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -1,6 +1,29 @@
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+
 export default function Price() {
 
+    const apiKey = 'pk_9155727789334028bb69df5beb1e5ceb'
+    const { symbol } = useParams()
+    const url = `https://cloud.iexapis.com/stable/crypto/${symbol}/price?token=${apiKey}`
+
+    const [coin, setCoin] = useState(null)
+
+    async function getCoin() {
+        const data = await fetch(url).then(res => res.json())
+        setCoin(data)
+    }
+
+    useEffect(() => {getCoin()}, [])
+    
+    const loaded = () => 
+            <div>
+                <h1>{symbol}</h1>
+                <h2>{coin.price}</h2>
+            </div>
+
+    const loading = () => <h1>Loading....</h1>
     return (
-        <h1>This is the Price component</h1>
+        coin ? loaded() : loading()
     )
 }
