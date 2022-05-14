3912c4ca582fb9b491fab5daa9d7d62420e419e9
Kyle Canamar <canamar.kyle@gmail.com>
2022-05-14 11:20:33 -0700

    adds Nav with working links

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Routes } from "react-router-dom"
 import Currencies from "./pages/Currencies"
 import Main from "./pages/Main"
 import Price from "./pages/Price"
+import Nav from "./components/Nav"
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Routes>
         <Route path="/" element={<Main/>}/>
         <Route path="/currencies" element={<Currencies/>}/>
