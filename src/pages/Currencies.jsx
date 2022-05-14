3462a45564ee4448ab95cfc4abb57e0f7815632b
Kyle Canamar <canamar.kyle@gmail.com>
2022-05-14 12:26:43 -0700

    creates working paths to each currency

--- src/pages/Currencies.jsx
+++ src/pages/Currencies.jsx
@@ -1,6 +1,22 @@
+import { Link } from "react-router-dom"
+
 export default function Currencies() {
 
+    const currencies = [
+        { name: "Bitcoin", symbol: "BTCUSD" },
+        { name: "Litecoin", symbol: "LTCUSD" },
+        { name: "Ethereum", symbol: "ETHUSD" },
+        { name: "Zcash", symbol: "ZECUSD" },
+        { name: "Bitcoin Cash", symbol: "BCHUSD" },
+      ]
+
     return (
-        <h1>This is the Currencies component</h1>
+        <div className='currencies'>
+            {currencies.map(({ name, symbol }, idx) => 
+                <Link key={idx} to={`/price/${symbol}`}>
+                    <h2>{name}</h2>               
+                </Link>
+            )}
+        </div>
     )
 }
